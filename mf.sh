35d9855b678ed6c631346e12cd1e5cbb14ce82ef
Chris Grimmett <chris@grimtech.net>
2019-09-27 18:29:28 -0700

    clean

--- mf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bindir="$(dirname "$(readlink -fn "$0")")"
-source "${bindir}/.env"
-
-cd "${bindir}"
-#espeak "firefoxy node binary path ${NODE_BINARY_PATH}"
-espeak "Adding magnetlink" &
-espeak $("${NODE_BINARY_PATH}" ./index.js "${1}" | jq .msg | cut -d '-' -f2)
